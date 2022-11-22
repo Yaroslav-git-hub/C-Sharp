eaed263ea9021652580d4b0c2430f869c273d294
Yaropolk <krive1ka.in0000@gmail.com>
2022-11-22 16:52:16 +0300

    File Done

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,28 +1,26 @@
-﻿//? Задача 34: 
-// Задайте массив заполненный случайными положительными трёхзначными числами. 
-// Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿//? Задача 36: 
+// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
-//! [345, 897, 568, 234] -> 2
+//* [3, 7, 23, 12] -> 19
+//* [-4, -6, 89, 6] -> 0
 
 Console.Write("Введите кол-во элементов в массиве: ");
 int el = Convert.ToInt32(Console.ReadLine());
 
-int[] NameArray(int element)
+int [] NameArray (int element)
 {
-    int[] array = new int[el];
     Random random = new Random();
-
+    int [] array = new int [element];
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = random.Next(100,1000);
-
+        array[i] = random.Next(1,10);
     }
     return array;
 }
 
 int [] array = NameArray(el);
 
-void PrintArray(int[] arr)
+void PrintArray(int [] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
@@ -44,20 +42,20 @@ void PrintArray(int[] arr)
 PrintArray(array);
 
 
-int SearchNumber (int [] arr)
+int SumArray (int [] arr)
 {
-    int count = 0;
+    int sum = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i] % 2 == 0)
+        if (i % 2 == 1)
         {
-            count++;
+            sum += arr[i];
         }
     }
 
-    return count;
+    return sum;
 }
 
-int res = SearchNumber(array);
 
-Console.Write($" -> {res}");
+int sum = SumArray(array);
+Console.Write($" -> {sum}");
---
+++ Task38/Program.cs
@@ -0,0 +1,66 @@
+﻿//? Задача 38: 
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+
+//* [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+Console.Write("Введите кол-во элементов в массиве: ");
+int el = Convert.ToInt32(Console.ReadLine());
+
+double[] NameArray(int element)
+{
+    Random random = new Random();
+    double[] array = new double[element];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round(random.NextDouble() * (100 - 10) + 10, 2, MidpointRounding.ToZero);
+    }
+    return array;
+}
+
+double[] array = NameArray(el);
+
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0)
+        {
+            Console.Write($"[{arr[i]} , ");
+        }
+        if (i > 0 && i < arr.Length - 1)
+        {
+            Console.Write($"{arr[i]} , ");
+        }
+        if (i > arr.Length - 2)
+        {
+            Console.Write($"{arr[i]}]");
+        }
+    }
+}
+
+PrintArray(array);
+
+
+double SumArray(double[] arr)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+    }
+    double res = max - min;
+
+    return res;
+}
+
+
+double res = Math.Round(SumArray(array), 2, MidpointRounding.ToZero);
+Console.Write($" -> {res}");
