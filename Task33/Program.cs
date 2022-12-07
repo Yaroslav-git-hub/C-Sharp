ec504f1596f1cb26dc8b52a48515b7c5515fff84
Yaropolk <krive1ka.in0000@gmail.com>
2022-12-08 01:02:43 +0300

    File Done

---
+++ Task33/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+Console.Write("Введите кол-во элементов: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальный промежуток: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальный промежуток: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число, которое хотите найти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+
+int[] NameArray(int n, int mn, int mx)
+{
+    int[] array = new int[n];
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(mn, mx + 1);
+    }
+
+    return array;
+}
+
+int[] array = NameArray(size, min, max);
+
+
+void SearchArray(int[] mas, int num)
+{
+    int res = 0;
+
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (mas[i] == num)
+        {
+            res = 1;
+        }
+        else
+        {
+            res = 0;
+        }
+    }
+
+    if (res == 1)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+
+
+void Mas(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0)
+        {
+            Console.Write($"[{arr[i]}, ");
+        }
+        if (i > 0 && i < arr.Length - 1)
+        {
+            Console.Write($"{arr[i]}, ");
+        }
+        if (i > arr.Length - 2)
+        {
+            Console.Write($"{arr[i]}]");
+        }
+    }
+}
+
+Mas(array);
+SearchArray(array, number);
+
---
+++ Task39/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 39: Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+
+
+
+Console.Write("Введите кол-во элементов в массиве: ");
+int el = Convert.ToInt32(Console.ReadLine());
+
+int [] NameArray (int element)
+{
+    Random random = new Random();
+    int [] array = new int [element];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(1,10);
+    }
+    return array;
+}
+
+
+int [] array = NameArray(el);
+
+void PrintArray(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0)
+        {
+            Console.Write($"[{arr[i]}, ");
+        }
+        if (i > 0 && i < arr.Length - 1)
+        {
+            Console.Write($"{arr[i]}, ");
+        }
+        if (i > arr.Length - 2)
+        {
+            Console.Write($"{arr[i]}]");
+        }
+    }
+}
+
+
+void ArrayReverse(int [] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int a = arr[i];
+        array[i] = array[array.Length - 1 -i];
+        array[array.Length - 1 -i] = a;
+    }
+}
+
+PrintArray(array);
+ArrayReverse(array);
+PrintArray(array);
